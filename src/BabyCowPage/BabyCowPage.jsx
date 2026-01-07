0d65e2463d98698c0b50ce616918f9d82a9c6fc7
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-01-08 00:46:42 +0100

    update buttons

--- src/BabyCowPage/BabyCowPage.jsx
+++ src/BabyCowPage/BabyCowPage.jsx
@@ -24,66 +24,50 @@ function BabyCowPage() {
       },
       body: JSON.stringify({
         prompt:
+
           "2D flat deformed derpy anime pink cute baby cow creepy with evil eyes and bloody knife and wearing" +
           likes.join(", "),
       }),
     });
     const data = await response.json();
+    const allButtons = document.querySelectorAll("Button");
     setImage(data.image);
     setLoading(false);
     setLikes([]);
+
+    allButtons.forEach((button) => {
+      button.addEventListener('click', function() {
+        this.classList.toggle('clicked');
+      });
+    });
   }
 
   return (
     <div id="BabyCowPage">
-      <h1>
-        {" "}
-        Lonely? Tell us what you like and we'll find a friend just for you!
-      </h1>
+
+      <h1> Lonely? Tell us what you like and we'll find a friend just for you!</h1>
 
       <div id="PreferenceButtons">
-        <button
-          className={`image-button ${
-            likes.includes("Strawberry") ? "clicked" : ""
-          }`}
-          onClick={() => toggleLike("Strawberry")}
-        >
-          <img
-            src="./assets/strawberry.png"
-            alt="Strawberry"
-            width="100"
-            height="100"
-          />
+        <button className="Button" onClick={() => setLikes([...likes, "Strawberry"])}>
+          <img src="./assets/strawberry.png" alt="Strawberry" width="100" height="100"/>
           Strawberry
         </button>
-        <button id="Button" onClick={() => setLikes([...likes, "Spaghetti"])}>
-          <img
-            src="./assets/spaghetti.png"
-            alt="Spaghetti"
-            width="100"
-            height="100"
-          />
+        <button className="Button" onClick={() => setLikes([...likes, "Spaghetti"])}>
+          <img src="./assets/spaghetti.png" alt="Spaghetti" width="100" height="100"/>
+
           Spaghetti
         </button>
-        <button id="Button" onClick={() => setLikes([...likes, "Steak"])}>
+        <button className="Button" onClick={() => setLikes([...likes, "Steak"])}>
           Steak
         </button>
-        <button id="Button" onClick={() => setLikes([...likes, "Boba Tea"])}>
+        <button className="Button" onClick={() => setLikes([...likes, "Boba Tea"])}>
           Boba
         </button>
-        <button id="Button" onClick={() => setLikes([...likes, "Pizza"])}>
-          <img
-            src="./assets/pizza.png"
-            alt="Spaghetti"
-            width="100"
-            height="100"
-          />
+
+        <button className="Button" onClick={() => setLikes([...likes, "Pizza"])}>
           Pizza
         </button>
-        <button
-          id="Button"
-          onClick={() => setLikes([...likes, "With 9 tiny baby cows"])}
-        >
+        <button className="Button" onClick={() => setLikes([...likes, "With 9 tiny baby cows"])}>
           Baby Cows
         </button>
       </div>
@@ -92,15 +76,11 @@ function BabyCowPage() {
         {loading ? "Your friend is coming..." : "Meet your new friend :3"}
       </button>
 
-      <img
-        id="Cow"
-        src={
-          loading ? "./assets/loading.gif" : image ? image : "./assets/cow.png"
-        }
-        alt="Baby Cow"
-      />
+
+      <img id="Cow" src={loading? "./assets/loading.gif" : (image ? image : "./assets/cow.png")} alt="Baby Cow" />
 
       <button onClick={() => window.location.reload()} id="HomeButton">
+        this.classList.add('clicked');
         Bye bye
       </button>
     </div>

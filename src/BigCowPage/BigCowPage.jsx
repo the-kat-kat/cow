5d8c59f49b20f34257060f7d9015d691286c0a76
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-01-07 09:33:07 +0000

    Add jumpscare?

--- src/BigCowPage/BigCowPage.jsx
+++ src/BigCowPage/BigCowPage.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react'
 
-function BigCowPage(props) {
-    return (
-        <div>
-            <button onClick={() => props.setPageFunction("home")}>back</button>
-            <p>this is big cow page</p>
+import './BigCowPage.css'
+
+
+function Jumpscare() {
+    return(
+        <div id="Jumpscare">
+            <p>boooooooooo</p>
         </div>
     )
+}
+
+function BigCowPage(props) {
+    const [BigCowStage, setBigCowStage] = useState("jumpscare")
+
+    if (BigCowStage == "jumpscare") {
+        return <Jumpscare />
+    } else {
+        return (
+            <div>
+                <button onClick={() => props.setPageFunction("home")}>back</button>
+                <p>this is big cow page</p>
+                <Jumpscare />
+            </div>
+        )
+    }
+
 }
 
 export default BigCowPage;

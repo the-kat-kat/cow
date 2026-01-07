090da2093864adc9e002c74a29678ad1247befc8
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-01-07 22:53:39 +0100

    add buttons

--- src/BabyCowPage/BabyCowPage.jsx
+++ src/BabyCowPage/BabyCowPage.jsx
@@ -1,11 +1,3 @@
-/**
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 import "./BabyCowPage.css";
 import { useState } from "react";
@@ -14,6 +6,7 @@ import { useState } from "react";
 function BabyCowPage() {
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [likes, setLikes] = useState([]);
 
   async function generateImage() {
     setLoading(true);
@@ -22,7 +15,10 @@ function BabyCowPage() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ prompt: "anime pink cute baby cow creepy with evil eyes and bloody knife" }),
+      body: JSON.stringify({
+        prompt:
+          "anime pink cute baby cow creepy with evil eyes and bloody knife",
+      }),
     });
     const data = await response.json();
     setImage(data.image);
@@ -31,18 +27,23 @@ function BabyCowPage() {
 
   return (
     <div id="BabyCowPage">
-    <h1> A friend just for you!</h1>
-    
-    <button id="GenerateCow" onClick={generateImage} disabled={loading}>
-        {loading ? "Generating..." : "Meet your new baby cow friend :3"}
-    </button>
+      <h1> A friend just for you!</h1>
 
-    <img
-        id="Cow"
-        src={image ? image : "./assets/cow.jpeg"}
-        alt="Baby Cow"
-      />
-    <img id="Steak" src="./assets/cow.jpeg" alt="Baby Steak" />
+      <div id="PreferenceButtons">
+        <button id="Button" onClick={() => setLikes([...likes, "Strawberry"])}>Strawberry</button>
+        <button id="Button" onClick={() => setLikes([...likes, "Spaghetti"])}>Spaghetti</button>
+        <button id="Button" onClick={() => setLikes([...likes, "Steak"])}>Steak</button>
+        <button id="Button" onClick={() => setLikes([...likes, "Yogurt"])}>Yogurt</button>
+        <button id="Button" onClick={() => setLikes([...likes, "Grass"])}>Grass</button>
+        <button id="Button" onClick={() => setLikes([...likes, "Cookies"])}>Cookies</button>
+      </div>
+
+      <button id="GenerateCow" onClick={generateImage} disabled={loading}>
+        {loading ? "Generating..." : "Meet your new baby cow friend :3"}
+      </button>
+
+      <img id="Cow" src={image ? image : "./assets/cow.jpeg"} alt="Baby Cow" />
+      <img id="Steak" src="./assets/cow.jpeg" alt="Baby Steak" />
     </div>
   );
 }
